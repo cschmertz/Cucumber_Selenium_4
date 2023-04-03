67b48cdca794fb73a1fe742e9e3a86e25250d64f
Riaz11-coder <64184169+Riaz11-coder@users.noreply.github.com>
2023-04-03 01:02:10 -0400

    push

--- src/test/java/stepDefinitions/BookStoreLogin.java
+++ src/test/java/stepDefinitions/BookStoreLogin.java
@@ -42,6 +42,6 @@ public class BookStoreLogin {
         browserUtils.getScreenshot("bookstore dashboard");
         browserUtils.scrollToElement(loginPage.logOutButton);
         loginPage.logOut();
-        System.out.println("Test successfully completed");
+        System.out.println("Test passed");
     }
 }
